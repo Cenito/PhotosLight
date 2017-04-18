c32cfaeba81b26a866bf9a8d463cbca42cd54a25
a-hestrm <a-hestrm@microsoft.com>
2017-04-18 14:58:30 +0200

    Added MenuFlyoutItem

---
+++ PhotosLight/Controls/MenuFlyoutItemEx.cs
@@ -0,0 +1,37 @@
+﻿using PhotosLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace PhotosLight.Controls
+{
+    internal class MenuFlyoutItemEx : MenuFlyoutItem
+    {
+        public static readonly DependencyProperty IconBackgroundProperty = DependencyProperty.Register(
+            "IconBackground",
+            typeof(Brush),
+            typeof(MenuFlyoutItemEx),
+            new PropertyMetadata(null));
+
+        public Brush IconBackground
+        {
+            get { return (Brush)GetValue(IconBackgroundProperty); }
+            set { SetValue(IconBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            "Description",
+            typeof(string),
+            typeof(MenuFlyoutItemEx),
+            new PropertyMetadata(null));
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+    }
+}
---
+++ PhotosLight/Converters/StringNullOrEmptyVisibillityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace PhotosLight.Converters
+{
+    public class StringNullOrEmptyVisibillityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+            if (value is string)
+                return string.IsNullOrEmpty(value as string) ? Visibility.Collapsed : Visibility.Visible;
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
